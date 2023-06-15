e48e2ebf3cb8ad7fa5dd4b7351b459e4e226d590
Ben Tutheridge <270209647@yoobeestudent.ac.nz>
2023-06-15 13:27:50 +1200

    Added admin pages and main content window, updated button click events and added associated pages

---
+++ Library Application/AdminMainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+
+namespace Library_Application
+{
+
+    public partial class AdminMainWindow : Window
+    {
+
+        public AdminMainWindow()
+        {
+
+            InitializeComponent();
+
+            AdminMainFrame.Content = new OverdueBooks();
+
+            // Place class in main window context
+            this.DataContext = new UserDetails();
+
+        }
+
+        public class UserDetails
+        {
+            public string imageSourceUrl { get; set; }
+            public string userFullName { get; set; }
+            public UserDetails()
+            {
+                // Set the URL for the image source
+                imageSourceUrl = "https://via.placeholder.com/150x150";
+
+                string userFirstName = "Ben";
+                string userLastName = "Tutheridge";
+                userFullName = userFirstName + " " + userLastName;
+            }
+        }
+
+        private void Overdue_click(object sender, RoutedEventArgs e)
+        {
+            AdminMainFrame.Content = new OverdueBooks();
+        }
+
+        private void EditContent_Click(object sender, RoutedEventArgs e)
+        {
+            AdminMainFrame.Content = new EditLibraryContent();
+        }
+    }
+}
