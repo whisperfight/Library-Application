38a4e51a72f9605dae710d4677e8417bccc03742
nipungoyal5165 <120641940+nipungoyal5165@users.noreply.github.com>
2023-06-20 14:41:32 +1200

    Update LoanItem.cs

--- Library Application/LoanItem.cs
+++ Library Application/LoanItem.cs
@@ -6,35 +6,41 @@ using System.Threading.Tasks;
 
 namespace Library_Application
 {
+    // Represents a loan item in the library application
     public class LoanItem
     {
-        string _imgurl = "";
-        string _title = "";
-        string _genre = "";
-        string _loanState = "";
+        string _imgurl = "";  // The URL of the loan item's image
+        string _title = "";  // The title of the loan item
+        string _genre = "";  // The genre of the loan item
+        string _loanState = "";  // The loan state of the loan item
 
+        // The URL of the loan item's image
         public string imgURL
         {
             get { return _imgurl; }
             set { _imgurl = value; }
         }
 
+        // The title of the loan item
         public string title
         {
             get { return _title; }
             set { _title = value; }
         }
 
+        // The genre of the loan item
         public string genre
         {
             get { return _genre; }
             set { _genre = value; }
         }
+
+        // The loan state of the loan item
         public string loanstate
         {
             get
             {
-                return _loanState;
+                return _loanState ;
             }
             set
             {
