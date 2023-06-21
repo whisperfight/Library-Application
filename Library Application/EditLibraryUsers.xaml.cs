0f0cfafd7cca93402101d75c00aa07a5160aa1a5
bgtuth <bentutheridge@gmail.com>
2023-06-21 12:02:16 +1200

    Tried to create a LINQ query that combines multiple tables to get a total of overdue books and total loaned books

--- Library Application/EditLibraryUsers.xaml.cs
+++ Library Application/EditLibraryUsers.xaml.cs
@@ -42,47 +42,91 @@ namespace Library_Application
 
         public void LoadDatabase()
         {
+
             //using (var db = new DataContext())
-            //{
-            //    var LibraryUsers = (from l in db.Loans
-            //                        join u in db.Users on l.UserID equals u.ID
-            //                        select new
-            //                        {
-            //                            u.ID,
-            //                            u.Username,
-            //                            u.FirstName,
-            //                            u.LastName,
-            //                            u.IsAdmin,
-            //                            u.IsEnabled,
-            //                            l.DueDate
-
-            //                        }).ToList().GroupBy(
-            //                            x => x.ID,
-            //                            (key, g) =>
-            //                            new UserList
-            //                            {
-            //                                ID = key,
-            //                                UserName = g.ToList().First().Username,
-            //                                FirstName = g.ToList().First().FirstName,
-            //                                LastName = g.ToList().First().LastName,
-            //                                IsAdmin = g.ToList().First().IsAdmin,
-            //                                IsEnabled = g.ToList().First().IsEnabled,
-            //                                IssuedCount = g.ToList().Count(),
-            //                                OverdueBookCount = g.ToList().Where(s => DateTime.Compare(s.DueDate, DateTime.Now) < 0).Count()
-            //                            }
-            //                        ).ToList();
-
-            //    listData = LibraryUsers;
-            //}
-
-            using (var db = new DataContext())
             {
-                var LibraryUsers = (from u in db.Users
-                        
-                                    //join l in db.Loans on u.ID equals l.UserID
-                                    select
 
-                                    new UserList
+                //Perform two separate queries one to retrieve the loan count and another to retrieve the overdue book count
+                //Then join between the Users table and the results of userLoans and userBooks to obtain the desired information in the libraryUsers query.
+
+                // Retrieve loaned items based on matching user table "ID" with the loaned items table "User ID"
+                var userLoans = (from u in db.Users
+                                 join l in db.Loans on u.ID equals l.UserID into loans
+                                 from loan in loans
+                                 select new //Select user book
+                                 {
+                                     UserId = u.ID, // Assign user to book
+                                     BookID = loan.BookID, // Retrieve Users loaned book ID
+                                     BookDueDate = loan.DueDate, // Retrieve Users loaned book due date
+                                 }).ToList();
+
+
+
+                //// Retrieve loaned items based on matching user table "ID" with the loaned items table "User ID"
+                //var userBookCount = (from u in db.Users
+                //                    join l in db.Loans on u.ID equals l.UserID into loans
+                //                     select new //Select user book
+                //                     {
+                //                         UserID = u.ID, // Assign user to book
+                //                         BooksIssued = loans.Count(),
+
+                //                     }).ToList();
+
+
+                //// Debug
+                //foreach (var loan in userLoans)
+                //{
+                //    Console.WriteLine("User ID = " + loan.UserId);
+                //    Console.WriteLine("Loan ID = " + loan.BookID);
+                //    Console.WriteLine("Issue date = " + loan.BookDueDate);
+
+                //    int issuedCount = userLoans.Count();
+                //}
+
+
+                // Retrieve book due-date information based on referencing the loan table "BookID" with the books table "ID"
+                var userBooks = (from ul in userLoans
+                                 join b in db.Books on ul.BookID equals b.ID
+                                 join u in db.Users on ul.UserId equals u.ID
+                                 select new
+                                 {
+                                     UserID = u.ID,
+                                     BookTitle = b.Title,
+                                     BookDueDate = ul.BookDueDate
+                                  }).ToList();
+
+
+                //Debug
+                foreach (var book in userBooks)
+                {
+                    Console.WriteLine("User = " + book.UserID);
+                    Console.WriteLine("Title = " + book.BookTitle);
+
+                    int overdueCount = userBooks.Count(b => DateTime.Compare(b.BookDueDate, DateTime.Now) < 0);
+
+                    Console.WriteLine("Overdue count = " + overdueCount);
+                }
+
+
+                //var libraryUsers = (from ul in userLoans
+                //                    join ub in userBooks on ul.UserId equals ub.UserId
+                //                    join u in db.Users on ul.UserId equals u.ID
+                //                    select new UserList
+                //                    {
+                //                        ID = u.ID,
+                //                        UserName = u.Username,
+                //                        FirstName = u.FirstName,
+                //                        LastName = u.LastName,
+                //                        IsAdmin = u.IsAdmin,
+                //                        IsEnabled = u.IsEnabled,
+                //                        //IssuedCount = userLoans.Count(),
+                //                        //OverdueBookCount = userBooks.Count(b => DateTime.Compare(b.BookDueDate, DateTime.Now) < 0) 
+                //                    }).ToList();
+
+
+                var libraryUsers = (from u in db.Users
+                                    //join ubc in userBookCount on u.ID equals ubc.UserID
+                                    select new UserList
                                     {
                                         ID = u.ID,
                                         UserName = u.Username,
@@ -90,13 +134,16 @@ namespace Library_Application
                                         LastName = u.LastName,
                                         IsAdmin = u.IsAdmin,
                                         IsEnabled = u.IsEnabled,
-                                        //IssuedCount = l.ID,
-                                        //OverdueBookCount = g.ToList().Where(s => DateTime.Compare(s.DueDate, DateTime.Now) < 0).Count()
-
-
+                                        //IssuedCount = ubc.BooksIssued,
+                                        //OverdueBookCount = userBooks.Count(b => DateTime.Compare(b.BookDueDate, DateTime.Now) < 0) 
                                     }).ToList();
 
-                listData = LibraryUsers;
+                listData = libraryUsers;
+            } //Check with Robert
+
+            using (var db = new DataContext)
+            {
+
             }
 
         }
