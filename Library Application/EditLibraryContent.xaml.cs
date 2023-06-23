ce70d962d3b1b8811d59e42a3183c9ebf0d9c4bf
Ben <bentutheridge@gmail.com>
2023-06-23 15:19:29 +1200

    Bens branch (#4)
    * Removed search button, increase search bar width
    
    * Added admin pages and main content window, updated button click events and added associated pages
    
    * Updated dropdown visual styles
    
    * Sorted out data access and entry removal on overdue books page
    
    * Completed overdue books sorting page
    
    * Working on implementing user profile and name display
    
    * Completed user details loading
    
    * Working on Edit user window
    
    * Completed design of add/edit book content window
    
    * Add user logic
    
    * Fixed combobox logic for adding users
    
    * Completed adding user window logic
    
    * Lots of changes to user window and list logic
    
    * Tried to create a LINQ query that combines multiple tables to get a total of overdue books and total loaned books
    
    * Fixed issue when loading user list would hide users without current books on loan
    
    * Updated user sorting logic
    
    * Fixed edit user mode logic
    
    * Complete user admin page, updated a few minor layout details
    
    * Fixed content list
    
    * Fixed content list view and sorting logic
    
    * Edit library content logic completed
    
    * Completed content logic
    
    * BG Colour

---
+++ Library Application/EditLibraryContent.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Library_Application
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class EditLibraryContent : Page
+    {
+
+        public List<Book> listData = new List<Book>();
+
+        public EditLibraryContent()
+        {
+            InitializeComponent();
+
+            LoadDatabase();
+            SortByID(listData); 
+            DisplayListData(listData);
+
+        }
+
+        public void DisplayListData(List<Book> data)
+        {
+
+            //Display number of listing/sort results
+            int resultsCount = listData.Count();
+            ResultsCounter.Text = "Showing " + resultsCount.ToString() + " results";
+
+            ListView ContentListControl = this.ContentListControl;
+            ContentListControl.ItemsSource = data;
+        }
+
+        public void SortByID(List<Book> input)
+        {
+            listData = input.OrderBy(item => item.ID).ToList();
+            DisplayListData(listData);
+        }
+
+        public void SortTitleByAtoZ(List<Book> input)
+        {
+            listData = input.OrderBy(item => item.Title).ToList();
+            DisplayListData(listData);
+        }
+
+        public void SortTitleByZtoA(List<Book> input) 
+        {
+            listData = input.OrderByDescending(item => item.Title).ToList();
+            DisplayListData(listData);
+        }
+
+        public void SortByRating(List<Book> input)
+        {
+            listData = input.OrderByDescending(item => item.Rating).ToList();
+            DisplayListData(listData);
+        }
+
+        public void LoadDatabase()
+        {
+            using (var db = new DataContext())
+            {
+                var catalog = (from b in db.Books
+                               select new Book
+                               {
+                                   ID = b.ID,
+                                   Title = b.Title,
+                                   Author = b.Author,
+                                   Summary = b.Summary,
+                                   TimeToRead = b.TimeToRead,
+                                   Rating = b.Rating,
+                                   NewRelease = b.NewRelease,
+                                   GenreTags = b.GenreTags,
+                                   CoverImageURL = b.CoverImageURL,
+                                   AvailableToLoan = b.AvailableToLoan,
+                                   DueDate = b.DueDate
+                               }).ToList();
+
+                listData = catalog;
+            }
+        }
+
+        private void SortByDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+
+            // Check if an item is selected
+            if (comboBox.SelectedItem != null)
+            {
+                var selectedItem = (ComboBoxItem)comboBox.SelectedValue;
+
+                // Get the content of the selected item
+                var selectedContent = selectedItem.Content;
+
+                // Perform actions based on the selected item
+                switch (selectedContent)
+                {
+                    case "Book ID number":
+                        SortByID(listData);
+                        break;
+                    case "Title A to Z":
+                        SortTitleByAtoZ(listData);
+                        break;
+                    case "Title Z to A":
+                        SortTitleByZtoA(listData);
+                        break;
+                    case "Rating":
+                        SortByRating(listData);
+                        break;
+                    default:
+                        ;
+                        break;
+                }
+            }
+        }
+
+        private void AddNewBook_Click(object sender, RoutedEventArgs e)
+        {
+            int editMode = 1; // Sets window to add new book edit mode
+
+            ContentWindow contentWindow = new ContentWindow(editMode, 0);
+
+            // Handle the Closing event
+            contentWindow.Closing += ContentWindow_Closing; ;
+            contentWindow.ShowDialog(); // Show the new window as a modal dialog
+        }
+
+
+
+        private void EditSelected_Click(object sender, RoutedEventArgs e)
+        {
+            Book selectedBook = (Book)ContentListControl.SelectedItem;
+
+            if (selectedBook != null)
+            {
+                int editMode = 2; // Sets window to add edit selected book mode
+                int selUserID = selectedBook.ID;
+
+                ContentWindow contentWindow = new ContentWindow(editMode, selUserID);
+
+                // Handle the Closing event
+                contentWindow.Closing += ContentWindow_Closing;
+                contentWindow.ShowDialog(); // Show the new window as a modal dialog
+            }
+        }
+
+
+
+        private void ContentWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Method to execute when the content window is closing, refresh listview data
+            LoadDatabase();
+            DisplayListData(listData);
+        }
+
+        private void RemoveEntry_Click(object sender, RoutedEventArgs e)
+        {
+            Book selectedItem = (Book)ContentListControl.SelectedItem;
+
+            // Check if an item is selected
+            if (selectedItem != null)
+            {
+
+                using (var db = new DataContext())
+                {
+                    Book deleteBook = new Book();
+
+                    deleteBook.ID = selectedItem.ID;
+                    db.Remove(deleteBook);
+                    db.SaveChanges();
+
+                }
+                // Refresh list control
+                LoadDatabase();
+                DisplayListData(listData);
+            }
+        }
+    }
+
+}
+
+
---
+++ Library Application/UserWindow.xaml.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library_Application
+{
+    /// <summary>
+    /// Interaction logic for AddMemberWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window, INotifyPropertyChanged
+    {
+
+        int editMode = 1; // 1 = add new member 2 = edit selected user
+        int selectedID;
+
+        public UserWindow(int mode, int ID)
+        {
+            editMode = mode;
+            selectedID = ID;
+
+            // Place class in main window context
+            this.DataContext = this;
+
+            InitializeComponent();
+
+            if (editMode == 2)
+            {
+                //Load and edit existing user
+                LoadSelectedUserData(selectedID);
+            }
+
+        }
+
+        private void LoadSelectedUserData(int userID)
+        {
+            using (var db = new DataContext())
+            {
+
+                List<User> selUser = db.Users.Where(x => x.ID == userID).ToList();
+
+
+                // Update text fields to selected member
+                FirstNameField.Text = selUser[0].FirstName;
+                LastNameField.Text = selUser[0].LastName;
+                UserNameField.Text = selUser[0].Username;
+                UserPasswordField.Text = selUser[0].Password;
+                ImageURLField.Text = selUser[0].ImageURL;
+
+                // Update selected combobox states
+
+                // Admin privs
+                if (selUser[0].IsAdmin == true)
+                {
+                    AdminTrue.IsSelected = true;
+
+                }
+                else
+                {
+                    AdminFalse.IsSelected = true;
+                }
+
+                if (selUser[0].IsEnabled == true)
+                {
+                    AccountEnabled.IsSelected = true;
+                }
+                else
+                {
+                    AccountDisabled.IsSelected = true;
+                }
+
+            }
+        }
+
+        private int GetNewPKFAddNewMember()
+        {
+            int newpkID = 0;
+
+            using (var db = new DataContext())
+            {
+                var userTable = db.Users.ToList();
+                newpkID = userTable.Count() + 1;
+            }
+ 
+            return newpkID;
+        }
+
+        // Test and update image button / property change event
+        private void btnTestImageLink_Click(object sender, RoutedEventArgs e)
+        {
+            ImageURL = ImageURLField.Text;
+        }
+        private string imageURL;
+        public string ImageURL
+        {
+            get { return imageURL; }
+            set
+            {
+                imageURL = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+        private void btnAddNewMember_Click(object sender, RoutedEventArgs e)
+        {
+
+            //Change logic based on content mode
+            switch (editMode)
+            {
+                case 1:
+                    // Add new user to database
+                    //Basic data validation to prevent empty user entries
+                    if (FirstNameField.Text != "" ||
+                        LastNameField.Text != "" ||
+                        UserNameField.Text != "" ||
+                        ImageURLField.Text != "")
+                    {
+                        using (var db = new DataContext())
+                        {
+                            User newUser = new User();
+
+                            // Call getPKF method to get new ID
+                            newUser.ID = GetNewPKFAddNewMember();
+                            newUser.FirstName = FirstNameField.Text;
+                            newUser.LastName = LastNameField.Text;
+                            newUser.Username = UserNameField.Text;
+                            newUser.Password = UserPasswordField.Text;
+                            newUser.IsAdmin = isAdmin;
+                            newUser.IsEnabled = isEnabled;
+                            newUser.ImageURL = ImageURLField.Text;
+
+                            db.Add(newUser);
+                            db.SaveChanges();
+
+                            ConfirmMessage.Text = "Changes saved!";
+                        }
+                    }
+                    else
+                    {
+                        ConfirmMessage.Text = "Missing data, Please fill out all entry fields!";
+                    }
+                    break;
+
+                case 2:
+                    // Edit selected user data and update records
+
+                    if (FirstNameField.Text != "" ||
+                       LastNameField.Text != "" ||
+                       UserNameField.Text != "" ||
+                       ImageURLField.Text != "")
+                    {
+                        using (var db = new DataContext())
+                        {
+                            int userId = selectedID; // Assign user ID from selected user
+
+                            // Retrieve the existing user entry from the database
+                            User existingUser = db.Users.FirstOrDefault(u => u.ID == userId);
+
+                            if (existingUser != null)
+                            {
+                                // Modify the properties of the existing user object
+                                existingUser.FirstName = FirstNameField.Text;
+                                existingUser.LastName = LastNameField.Text;
+                                existingUser.Username = UserNameField.Text;
+                                existingUser.Password = UserPasswordField.Text;
+                                existingUser.IsAdmin = isAdmin;
+                                existingUser.IsEnabled = isEnabled;
+                                existingUser.ImageURL = ImageURLField.Text;
+
+                                // Save the changes back to the database
+                                db.SaveChanges();
+
+                                ConfirmMessage.Text = "Changes saved!";
+                            }
+                            else
+                            {
+                                ConfirmMessage.Text = "User not found!";
+                            }
+                        }
+
+                        ConfirmMessage.Text = "Changes saved!";
+                    }
+                    else
+                    {
+                        ConfirmMessage.Text = "Missing data, Please fill out all entry fields!";
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        bool isAdmin = false;
+        bool isEnabled = false;
+
+        private void UserPrivilegesCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+
+            // Check if an item is selected
+            if (comboBox.SelectedItem != null)
+            {
+
+                ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+                string selectedContent = selectedItem.Name.ToString();
+
+                // Perform actions based on the selected item
+                switch (selectedContent)
+                {
+                    case "AdminTrue":
+                        // Handle admin privileges selection
+                        isAdmin = true;
+
+                        // Set current item as selected
+                        selectedItem.IsSelected = true;
+                        SelectPriv.IsSelected = false;
+                        break;
+                    case "AdminFalse":
+                        // Handle standard privileges selection
+                        isAdmin = false;
+
+                        // Set current item as selected
+                        selectedItem.IsSelected = true;
+                        SelectPriv.IsSelected = false;
+                        break;
+                    default:
+                        // Handle other selections or the default case
+                        SelectPriv.IsSelected = true;
+                        break;
+                }
+            }
+        }
+
+
+        private void UserEnabledCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+
+            // Check if an item is selected
+            if (comboBox.SelectedItem != null)
+            {
+                ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+                string selectedContent = selectedItem.Name.ToString();
+
+                // Perform actions based on the selected item
+                switch (selectedContent)
+                {
+                    case "AccountEnabled":
+                        // Handle admin privileges selection
+                        isEnabled = true;
+                        // Set current item as selected
+                        selectedItem.IsSelected = true;
+                        break;
+
+                    case "AccountDisabled":
+                        // Handle standard privileges selection
+                        isEnabled = false;
+                        // Set current item as selected
+                        selectedItem.IsSelected = true;
+                        break;
+                    default:
+                        // Handle other selections or the default case
+                        break;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
+
+
+
+
+
+
